c5c60801878bc534461bb59b871b7b9c1192c988
Rajat Mhetre <mhetrerajat@gmail.com>
2025-03-27 19:09:39 -0700

    refactor: reset dotfiles

--- .config/nvim/lua/plugins/comment.lua
+++
@@ -1,9 +0,0 @@
-return {
-	-- comment neovim plugins
-	'numToStr/Comment.nvim',
-	lazy = true,
-	event = { "CursorHold", "CursorHoldI" },
-	config = function()
-		require('Comment').setup()
-	end
-}
